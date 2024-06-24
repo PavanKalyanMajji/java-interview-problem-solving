37359fc34bb32aef9af3add64d172c7a4f9d8515
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-06-24 21:33:40 +0530

    reverse the number

---
+++ src/com/tcs/javaproblemsolving/ReversNumber.java
@@ -0,0 +1,24 @@
+package com.tcs.javaproblemsolving;
+/**
+ * 
+ * Reverse Given Number
+ * example1 :- 341
+ * output :- 143
+ * 
+ * example2 :- 421
+ * output :- 124
+ */
+public class ReversNumber {
+	public static int reversNumber(int value) {
+		int temp=0;
+		while(value%10>0) {
+			temp = temp*10 + value%10;
+			value = value/10;
+		}
+		return temp;
+	}
+	public static void main(String[] args) {
+		int value =341;
+		System.out.println(reversNumber(value));
+	}
+}
