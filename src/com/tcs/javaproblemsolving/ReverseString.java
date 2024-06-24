71b0756c207fc661bc579ef97bd03b3b48230f31
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-06-24 23:48:12 +0530

    Reverse the string

---
+++ src/com/tcs/javaproblemsolving/ReverseString.java
@@ -0,0 +1,16 @@
+package com.tcs.javaproblemsolving;
+/**
+ * 
+ *Reverse the given String 
+ *example:- "PavanKalyan" output:-naylaKnavap
+ *
+ */
+public class ReverseString {
+	public static void reverseTheString(String value) {
+		for(int i=value.length()-1;i>=0;i--)
+			System.out.print(value.charAt(i));
+	}
+	public static void main(String[] args) {
+		reverseTheString("PavanKalyan");
+	}
+}
