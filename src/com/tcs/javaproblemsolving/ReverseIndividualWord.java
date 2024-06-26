5ba3fc8a8f1093661c8f3fc6da38e928058eaab0
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-06-26 21:55:16 +0530

    Reverse individual word latest code

--- src/com/tcs/javaproblemsolving/ReverseIndividualWord.java
+++ src/com/tcs/javaproblemsolving/ReverseIndividualWord.java
@@ -1,7 +1,13 @@
 package com.tcs.javaproblemsolving;
 
 import java.util.Stack;
-
+/**
+ * 
+ *Reverse Individual Word
+ *example1: Hi Good Morning
+ *output: iH dooG gninroM
+ *
+ */
 public class ReverseIndividualWord {
 //	Hi MyName is pavan kalyan
 	public static void reverseIndividualWord(String word) {
@@ -22,7 +28,31 @@ public class ReverseIndividualWord {
 			System.out.print(stack.pop());	
 		}
 	}
+	
+	public static void reverseIndividualWordSecondApproch(String word) { 
+		int temp=-1,firstIndex=0,lastIndex=0,firstSpace=0;
+		for(int i=0;i<word.length();i++) {
+			if(word.charAt(i)==' ' || word.length()-1==i) {
+				firstSpace+=1;
+				lastIndex=i;
+				firstIndex=temp;
+				temp = lastIndex;
+				if(firstSpace ==1) {
+					lastIndex-=1;
+				}
+				while(lastIndex>firstIndex) {
+					if(word.length()-1==lastIndex) {
+						System.out.print(" ");
+					}
+					System.out.print(word.charAt(lastIndex));
+					lastIndex-=1;
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
-		reverseIndividualWord("Hi My Name is Pavan kalyan");
+//		reverseIndividualWord("Hi My Name is Pavan kalyan");
+//		System.out.println();
+		reverseIndividualWordSecondApproch("Hi Good Morning have a nice day");
 	}
 }
